634ebaa7aad16abad35d60a8ec32a70b387d2357
tchoedak <tchoedak@gmail.com>
2019-04-15 16:29:58 -0700

    fix a lot of things

--- tsdcli/cli.py
+++ tsdcli/cli.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import click
 
 from .commands import emr, git
@@ -11,3 +12,6 @@ cli.add_command(git.push)
 cli.add_command(git.merge)
 cli.add_command(emr.push_file)
 cli.add_command(emr.install_remotely)
+
+if __name__ == '__main__':
+    cli()
